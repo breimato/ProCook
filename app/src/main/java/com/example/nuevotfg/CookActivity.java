519cf1b7f701a0c24e4fa8d470d3087c385f9f8b
BreiMatoM <97673567+BreiMatoM@users.noreply.github.com>
2022-06-09 15:12:31 +0200

    Update 09/06/2022 :: 15:11 || Responde al click y envía el ID del plato

--- app/src/main/java/com/example/nuevotfg/CookActivity.java
+++ app/src/main/java/com/example/nuevotfg/CookActivity.java
@@ -1,5 +1,6 @@
 package com.example.nuevotfg;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -76,7 +77,37 @@ public class CookActivity extends AppCompatActivity implements RecyclerViewAdapt
 
     @Override
     public void onResultClick(int position) {
-        Log.d(TAG, "onResultClick: clicked");
+        OkHttpClient client = new OkHttpClient();
+        //GSON PARA PARSEAR EL JSON A OBJETO
+        Gson gson = new Gson();
+        //URL Y APIKEY DE LA API
+        String apiKey = "&apiKey=40c52ddeebce4a988c8472b08e9bc93b";
+        String url = "https://api.spoonacular.com/recipes/complexSearch?includeIngredients=onion,tomatoes";
+        Request request = new Request.Builder()
+                .url(url+apiKey)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()){
+                    String myResponse = response.body().string();
+                    CookActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Recipe r = gson.fromJson(myResponse, Recipe.class);
+                            Intent intent = new Intent(CookActivity.this, RecipeViewActivity.class);
+                            intent.putExtra(TAG, r.results.get(position).getId());
+                            startActivity(intent);
+                        }
+                    });
+                }
+            }
+        });
     }
 }
 
